4863a0437a098c0c828de4c1243dfcddac1540a0
Maxim Belkin <belkinurban@gmail.com>
2021-07-05 08:26:25 +0200

    feat: :sparkles: Start a server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+const start = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
