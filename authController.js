db1a6d5eb5c30c61205d14ab943935a96c3af930
Maxim Belkin <belkinurban@gmail.com>
2021-07-05 10:31:09 +0200

    feat: :sparkles: Add user login logic

--- authController.js
+++ authController.js
@@ -1,10 +1,27 @@
+require('dotenv').config();
+
+const JWS_SIGN_KEY = process.env.JWS_SIGN_KEY;
+
 const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
 
 const { validationResult } = require('express-validator');
 
 const User = require('./models/User');
 
 const Role = require('./models/Role');
+
+const generateAccessToken = (params) => {
+  const { id, roles, sign, expires } = params;
+
+  const payload = {
+    id,
+    roles,
+  };
+
+  return jwt.sign(payload, sign, { expiresIn: expires });
+};
 
 class AuthController {
   async registration(req, res) {
@@ -51,6 +68,33 @@ class AuthController {
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+
+      // Try find in ddbb. Return if not exists
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `Username ${username} is not exists!` });
+      }
+
+      // Validate password
+      const validPassword = bcrypt.compareSync(password, user.password);
+
+      if (!validPassword) {
+        return res.status(400).json({ message: `The password is not valid!` });
+      }
+
+      // Get jws token
+      const token = generateAccessToken({
+        id: user._id,
+        roles: user.roles,
+        sign: JWS_SIGN_KEY,
+        expires: '12h',
+      });
+
+      return res.json({ token });
     } catch (e) {
       console.error(e);
       res.status(400).json({ message: 'Login error!' });
